b86299a90451205fdbb742b159e8383299fd8afd
Van Tien Cong (FSU2.BU68) <CongVT2@fsoft.com.vn>
2018-11-01 20:34:36 +0700

    Add project files.

---
+++ NhatVe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NhatVe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
