3b495b65bb70340e910b01a2492eab061b9bddc0
HP 1040G1 <HP 1040G1@CONGVT2>
2018-11-08 23:14:52 +0700

    Tạo các interfaces UnitOfWork và Repositories

---
+++ NhatVe.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NhatVe.Infrastructure.Interfaces;
+
+namespace NhatVe.Data.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
